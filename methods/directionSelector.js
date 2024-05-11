46e81741aa15d1fb61b147ec22313c06ec42ac68
Kiana <kianakermani2001@gmail.com>
2024-05-10 22:57:50 -0700

    bug fixed

--- methods/directionSelector.js
+++ methods/directionSelector.js
@@ -1,15 +1,18 @@
-  //direction
-  $("#direction-selector").append("<option value='rtl'>راست به چپ</option>");
-  $("#direction-selector").append("<option value='ltr'>چپ به راست</option>");
-
-  $("#direction-selector").val("rtl");
-
-  $("#direction-selector").change(function () {
-    var theme = $(this).val();
-
-    if (theme === "rtl") {
-      $("body").removeClass("lDirection");
-    } else {
-      $("body").addClass("lDirection");
-    }
+$(document).ready(function () {
+    //direction
+    $("#direction-selector").append("<option value='rtl'>راست به چپ</option>");
+    $("#direction-selector").append("<option value='ltr'>چپ به راست</option>");
+  
+    $("#direction-selector").val("rtl");
+  
+    $("#direction-selector").change(function () {
+      var theme = $(this).val();
+  
+      if (theme === "rtl") {
+        $("body").removeClass("lDirection");
+      } else {
+        $("body").addClass("lDirection");
+      }
+    });
   });
+  
