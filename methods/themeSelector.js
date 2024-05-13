3cdc974134d69bd1d83292576ed642e45a5c17ae
kianakermani <kianakermani2001@gmail.com>
2024-05-13 04:40:24 -0700

    make code clean

--- methods/themeSelector.js
+++ methods/themeSelector.js
@@ -1,18 +1,15 @@
 $(document).ready(function () {
-  // Add options to the theme selector
   $("#theme-selector").append("<option value='light'>روشن</option>");
   $("#theme-selector").append("<option value='dark'>تیره</option>");
 
-  // Set the default theme
   $("#theme-selector").val("light");
 
-  // Handle theme change
   $("#theme-selector").change(function () {
     var theme = $(this).val();
     if (theme === "dark") {
-      $("#stylesheet").attr('href', './styles/dark.css');
+      $("#stylesheet").attr("href", "./styles/dark.css");
     } else {
-      $("#stylesheet").attr('href', './styles/light.css');
+      $("#stylesheet").attr("href", "./styles/light.css");
     }
   });
 });
